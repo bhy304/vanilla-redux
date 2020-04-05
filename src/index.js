6286e50790f3f027dbbcdd9a3d2d518c5d2adb57
bhy304 <bhy0512@gmail.com>
2020-04-05 14:44:41 +0900

    #1.1 Redux - Store and Reducer, Actions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-number.innerText = count;
+const reducer = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
-const updateText = () => {
-  number.innerText = count;
-}
+const countStore = createStore(reducer);
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-}
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-}
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
