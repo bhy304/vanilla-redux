933d0c137e5d267685bbc1b332f87f1787ecacea
bhy304 <bhy0512@gmail.com>
2020-04-05 15:35:17 +0900

    #1.3 code refactoring

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ const number = document.querySelector('span');
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const reducer = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count
   }
 };
 
@@ -26,11 +30,11 @@ const onChange= () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 
 }
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" })
+  countStore.dispatch({ type: MINUS })
 }
 
 add.addEventListener("click", handleAdd);
